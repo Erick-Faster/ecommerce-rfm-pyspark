2c03b60b1ecdee8be30b7db055b537ccf6a3d425
Erick-Faster <erick.fasterra@gmail.com>
2022-12-14 19:36:02 -0300

    Fix plot

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
-import seaborn as sns
 
 st.set_page_config(
     page_title="RFM Analysis",
